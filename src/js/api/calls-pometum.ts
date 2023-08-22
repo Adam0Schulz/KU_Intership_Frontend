081b4db659242dcc5ca9cfd1bc791720f62c9987
Adam0Schulz <adam@schulz.sk>
2023-08-22 11:37:30 +0200

    tried to add some back-to-front connection in the home page

--- src/js/api/calls-pometum.ts
+++ src/js/api/calls-pometum.ts
@@ -1,13 +1,14 @@
 
 // For Pometum Site (Apple Database)
 
+import { AxiosResponse } from "axios"
 import { api } from "./axios"
 import { Metadata, PometumSite } from "@js/interfaces"
 
-export const getAll = async (): Promise<PometumSite> => {
+export const getAll = async (): Promise<AxiosResponse<PometumSite, any>> => {
     return api.get("/content").then(res => res.data)
 }
 
-export const getMeta = async (): Promise<Metadata> => {
+export const getMeta = async (): Promise<AxiosResponse<Metadata, any>> => {
     return await api.get("/meta")
 }
--- src/pages/homePage/index.ts
+++ src/pages/homePage/index.ts
@@ -1,46 +1,35 @@
 import $ from 'jquery';
 import './style.css'
 import components from "@js/components";
-import { Address, PometumSite } from '@js/interfaces';
 
-$(function () {
+import { getMeta } from '@js/api/calls';
+const meta = getMeta().then(res => res.data)
+meta.then(res => console.log(res))
+
+
+$(async function () {
 
     components(
         {
             searchSection: {
-                heading: "Find Apple Species",
-                subHeading: "Search for apples by name"
+                heading: "Find " + (await meta).mainEntity,
+                subHeading: "Search for " + (await meta).mainEntity
             },
             branding: {
-                heading: "Apple database"
+                heading: (await meta).database
             },
             crumbsArray: [
                 { label: "Home", link: "", isActive: true }
             ],
             pageConfig: {
-                title: "Denmark's Apples",
+                title: (await meta).database,
                 pages: [
                     { pageTitle: 'Home', isActive: true },
                     { pageTitle: 'Browse', isActive: false },
                     { pageTitle: 'About', isActive: false }
                 ]
             },
-            contact: {
-                institution: 'Københavns Universitet',
-                institutionWeb: 'http://www.kommunikation.ku.dk',
-                department: 'Kommunikation',
-                departmentWeb: 'http://www.kommunikation.ku.dk',
-                address: {
-                    country: 'Denmark',
-                    city: 'København',
-                    district: 'K',
-                    postalCode: 1165,
-                    addressDetail: 'Nørregade 10'
-                },
-                team: 'Web Team',
-                email: 'FA-webredaktor@adm.ku.dk',
-                phone: 4535324261
-            }
+            contact: (await meta).contact
         }
     )
 
