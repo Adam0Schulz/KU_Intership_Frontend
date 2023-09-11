5815ef0cb8063c3438c8acfb709d8914c3c560f0
brrobert17 <br.robert17@gmail.com>
2023-09-12 00:15:06 +0200

    add filter option button, functionality v1

--- src/components/Input/AddFilterButton/index.ts
+++ src/components/Input/AddFilterButton/index.ts
@@ -1,16 +1,24 @@
 import $ from 'jquery';
 import HTML from './component.html';
 import './style.css';
+import TextInput from "@components/Input/Text";
+import RemoveIcon from "@components/Input/RemoveIcon";
+import InfoIcon from "@components/Input/InfoIcon";
 
 export default (options: string[])=> {
     const component = $(HTML);
     options.forEach(option => {
-        component.find('.dropdown-menu').append(`<li><a href="#">${option.toUpperCase()}</a></li>`);
+        component.find('.dropdown-menu').append(`<li><a href="#" id="id-${option}">${option.toUpperCase()}</a></li>`);
     })
-    $('div[add-filter-button]').replaceWith(component);
-    $('#add-filter-comp').on('click', 'li a', (e)=> {
-        console.log(e.currentTarget)
-        console.log('clicked menu')
+    $('div[add-filter-button]').append(component);
+    $('#add-filter-comp a').on('click', function (e) {
+        e.preventDefault();
+        console.log(`clicked:${this.id}`)
+        this.remove();
+        $('div[add-filter-button]').before($(`<div input-text label="${this.textContent}" placeholder="Search by ${this.textContent}"></div>`));
+        TextInput()
+        RemoveIcon()
+        InfoIcon()
     })
 
 }
