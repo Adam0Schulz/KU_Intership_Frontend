c17f09a7513aaafa657970789df727025b569c69
brrobert17 <br.robert17@gmail.com>
2023-09-27 10:50:13 +0200

    backend: mysql connection established .env fixed

--- Back-end/src/DatabaseConnection/connection.ts
+++ Back-end/src/DatabaseConnection/connection.ts
@@ -1,11 +1,12 @@
 import mysql from "mysql2/promise";
+import 'dotenv/config';
 import * as process from "process";
 
 
 const createConnection = () => {
     return mysql.createConnection({
         host: process.env.DATABASE_HOST,
-        port: 3306,
+        port: parseInt(process.env.DATABASE_PORT),
         database: process.env.DATABASE_DB_NAME,
         user: process.env.DATABASE_USERNAME,
         password: process.env.DATABASE_PASSWORD
